aad46a0e6053c201365d6b1266be3b310a9455c4
Héctor Esparza <hector.esparza.29@gmail.com>
2026-09-08 23:17:31 -0600

    Spring Profile
    Se agrega manejo de profiles dentro del aplicativo para ejecutar en diferentes ambientes  por ejemplo en ambiente dev

--- src/main/java/com/platzi/platzi_play/HelloController.java
+++ src/main/java/com/platzi/platzi_play/HelloController.java
@@ -1,18 +1,22 @@
 package com.platzi.platzi_play;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class HelloController {
     
+    private final String plataform;
+    
     private final PlatziPlayAiService aiService;
 
-    public HelloController(PlatziPlayAiService aiService) {
+    public HelloController(@Value("${spring.application.name}") String plataform, PlatziPlayAiService aiService) {
         this.aiService = aiService;
+        this.plataform = plataform;
     }
     @GetMapping("/")
     public String hello(){
-        return this.aiService.genereteGreeting();
+        return this.aiService.genereteGreeting(plataform);
     }
 }
--- src/main/java/com/platzi/platzi_play/PlatziPlayAiService.java
+++ src/main/java/com/platzi/platzi_play/PlatziPlayAiService.java
@@ -1,15 +1,16 @@
 package com.platzi.platzi_play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService 
 public interface PlatziPlayAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de Gestión de Peliculas PlatziPlay.
+            Genera un saludo de bienvenida a la plataforma de Gestión de Peliculas {{plataform}}.
             Usa menos de 120 caracteres y hazlo con el estilo de Platzi.
             """)
-    String genereteGreeting();
+    String genereteGreeting(@V("plataform") String plataform);
     
 }
