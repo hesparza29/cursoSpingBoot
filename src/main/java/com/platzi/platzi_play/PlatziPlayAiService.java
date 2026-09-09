5dcbb929450ea21e1a934e02f198b21bdf68c640
Héctor Esparza <hector.esparza.29@gmail.com>
2026-09-08 21:38:25 -0600

    Configuracion IA
    Se agrega langchain4j para integrar modelo de IA al proyecto

---
+++ src/main/java/com/platzi/platzi_play/PlatziPlayAiService.java
@@ -0,0 +1,15 @@
+package com.platzi.platzi_play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService 
+public interface PlatziPlayAiService {
+
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma de Gestión de Peliculas PlatziPlay.
+            Usa menos de 120 caracteres y hazlo con el estilo de Platzi.
+            """)
+    String genereteGreeting();
+    
+}
