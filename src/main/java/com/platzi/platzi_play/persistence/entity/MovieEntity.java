827c4a754f5048048ae180430a537c901f6078a2
Héctor Esparza <hector.esparza.29@gmail.com>
2026-09-09 20:20:16 -0600

    Se agrega Lombok
    Se agrega lombok y se crea la primera entidad de la capa de persistencia

---
+++ src/main/java/com/platzi/platzi_play/persistence/entity/MovieEntity.java
@@ -0,0 +1,44 @@
+package com.platzi.platzi_play.persistence.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity 
+@Table(name = "platzi_play_peliculas")
+@Data 
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Builder 
+@ToString 
+public class MovieEntity {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String titulo;
+    @Column(nullable = false, precision = 3)
+    private Integer duracion;
+    @Column(nullable = false, length = 10)
+    private String genero;
+    @Column(nullable = true, name = "fecha_estreno")
+    private LocalDate fechaEstreno;
+    @Column(nullable = true, precision = 3, scale = 2)
+    private BigDecimal clasificacion;
+    @Column(nullable = false, length = 1)
+    private String estado;
+    
+}
